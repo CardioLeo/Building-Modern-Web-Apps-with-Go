ae802cac4e71ce631b93313fbcf55458455d6c20
CardioLeo <CardioLeo@gmail.com>
2025-01-23 00:50:00 -0700

    adds more slices, selective members of slices, and prints them

--- 01-section/maps-and-slices.go
+++ 01-section/maps-and-slices.go
@@ -86,4 +86,16 @@ func main() {
 	sort.Ints(myIntSlice) // have to include/import sort
 
 	log.Println(myIntSlice)
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// shorthand declaration
+
+	log.Println(numbers)
+
+	log.Println(numbers[0:2])
+	log.Println(numbers[6:9])
+
+	names := []string{"one", "seven", "fish", "cat"}
+
+	log.Println(names)
 }
