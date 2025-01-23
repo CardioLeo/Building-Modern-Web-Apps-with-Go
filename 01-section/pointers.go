872a74758456bae749f7e17db8b70e765d30893f
CardioLeo <CardioLeo@gmail.com>
2025-01-22 23:58:00 -0700

    halfway through pointers video

---
+++ 01-section/pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	var myString string
+	myString = "Green"
+
+	log.Println("myString is set to", myString)
+	
+	changeUsingPointer(&myString) // using a reference
+
+	log.Println("myString is now set to", myString)
+
+}
+
+func changeUsingPointer(s *string) {
+	newValue := "Red"
+	*s = newValue
+}
