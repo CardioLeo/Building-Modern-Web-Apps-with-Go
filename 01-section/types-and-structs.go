f669a8ae125dce713024ea29109f09268cd2a8ba
CardioLeo <CardioLeo@gmail.com>
2025-01-23 00:04:53 -0700

    initial follow along code for types-and-structs video

---
+++ 01-section/types-and-structs.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+)
+
+var s = "seven"
+
+func main() {
+	var s2 = "six"
+	log.Println(s)
+	log.Println("s is", s)
+	log.Println("s2 is", s2)
+}
+
+func saySomething(s string) (string, string) {
+	return s, "World"
+}
