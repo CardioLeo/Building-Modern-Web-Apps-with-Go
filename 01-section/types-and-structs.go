24389aa207faad60f6b46000ae74a6bbd360b8d8
CardioLeo <CardioLeo@gmail.com>
2025-01-23 00:08:25 -0700

    mostly adds a few comments

--- 01-section/types-and-structs.go
+++ 01-section/types-and-structs.go
@@ -8,11 +8,14 @@ var s = "seven"
 
 func main() {
 	var s2 = "six"
-	log.Println(s)
+
+	s := "eight"
+
+	log.Println(s) // pulls on local var, not package level var
 	log.Println("s is", s)
 	log.Println("s2 is", s2)
 
-	saySomething("xxx")
+	saySomething("xxx") // pulls on package level var, not local var
 }
 
 func saySomething(s3 string) (string, string) {
