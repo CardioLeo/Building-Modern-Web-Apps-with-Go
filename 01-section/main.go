6ce72c499d9198f081913cecc1acc65115fb10c6
CardioLeo <CardioLeo@gmail.com>
2025-01-22 23:46:08 -0700

    adds comments and the i var as he uses it in the course

--- 01-section/main.go
+++ 01-section/main.go
@@ -8,6 +8,7 @@ func main() {
 	fmt.Println("Hello, World.")
 
 	var whatToSay string
+	var i int
 
 	whatToSay = "Goodbye, cruel world"
 
@@ -16,4 +17,10 @@ func main() {
 	whatToSay = "Sorry, I meant: 'Goodbye, relatively cruel world'"
 
 	fmt.Println(whatToSay)
+
+	// i = "cat" // doesn't work, he says, and I'm following along
+
+	i = 7
+
+	fmt.Println("i is set to", i)
 }
