9b5909a77e7a94b905b76b533a4c1d7e2c3edfdf
CardioLeo <CardioLeo@gmail.com>
2025-01-23 01:03:03 -0700

    adds if statements and bools; just over half of the lecture progressed through

--- 01-section/decision-structures.go
+++ 01-section/decision-structures.go
@@ -9,11 +9,31 @@ func main() {
 	// very like other languages
 
 	var isTrue bool
-	// isTrue = true
-	isTrue = false
+	// isTrue = false
+	isTrue = true
 	if isTrue {
 		log.Println("isTrue is", isTrue)
 	} else {
 		log.Println("isTrue is", isTrue)
 	}
+
+	cat := "cat"
+
+	if cat == "cat" {
+		log.Println("Cat is", cat)
+	} else {
+		log.Println("Cat is not", cat)
+	}
+
+	myNum := 100
+	isTrue2 := false
+
+	if (myNum > 99 && isTrue) { // deliberately not isTrue2
+		log.Println("myNum is greater than 99 and isTrue is set to", isTrue, "but isTrue2 is set to", isTrue2)
+	}
+
+	// he has a bunch more else conditionals - but I'm tired, it's late,
+	// and I've done this before. I'm going over this aspect mostly just for practice
+
+
 }
