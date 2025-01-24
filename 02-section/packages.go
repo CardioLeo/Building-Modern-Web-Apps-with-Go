0eb259b97cf3dc69bd8d7860bcca41d452c98e5f
CardioLeo <CardioLeo@gmail.com>
2025-01-24 13:38:25 -0700

    adds comment

--- 02-section/packages.go
+++ 02-section/packages.go
@@ -15,3 +15,4 @@ func main() {
 
 // "sometimes I have to fight with goland to get it to recognize it." ugh - this was my
 // I had no luck
+// have to enable it in GoLand; maybe not in VSCode -- what about VIM???
