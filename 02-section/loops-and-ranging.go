f530b0583f00b5137bda56117f8f3a89b2da3577
CardioLeo <CardioLeo@gmail.com>
2025-01-24 13:07:33 -0700

    adds ranging loop of animals, printed out

--- 02-section/loops-and-ranging.go
+++ 02-section/loops-and-ranging.go
@@ -14,4 +14,10 @@ func main() {
 	for i := 0; i <= 5; i++ {
 		log.Println(i)
 	}
+
+	animals := []string{"dog", "fish", "horse", "cow", "cat"}
+
+	for i, animal := range animals {
+		log.Println(i, animal)
+	}
 }
