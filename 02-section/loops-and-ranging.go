66f224eae0e2b1931ccaa65bd627109c13b9883b
CardioLeo <CardioLeo@gmail.com>
2025-01-24 13:20:12 -0700

    changes Poe poem line loop so it actually prints out the index of the letter and the letter; also adds a map of users, using user struct and range loop

--- 02-section/loops-and-ranging.go
+++ 02-section/loops-and-ranging.go
@@ -36,7 +36,24 @@ func main() {
 	var firstLine = "Once upon a midnight dreary"
 	for i, l := range firstLine {
 		log.Println(i, ":", l)
-		log.Printf("%c:%c", i, l)
+		log.Printf("%d:%c", i, l)
+	}
+
+	type User struct {
+		FirstName string
+		LastName string
+		Email string
+		Age int
+	}
+
+	var users []User
+	users = append(users, User{"John", "Smith", "john@smith.com", 30})
+	users = append(users, User{"Mary", "Jones", "mary@jones.com", 20})
+	users = append(users, User{"Sally", "Brown", "sally@brown.com", 45})
+	users = append(users, User{"Alex", "Anderson", "alex@smith.com", 17})
+
+	for _, l := range users {
+		log.Println(l.FirstName, l.LastName, l.Email, l.Age)
 	}
 
 }
