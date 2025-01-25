4118555514efffe595c9b00f49f9b483411f8f73
CardioLeo <CardioLeo@gmail.com>
2025-01-25 01:10:30 -0700

    updates the line inside handlefunc to work better, comments out fmt command

--- 03-basic-web-app/hello_world.go
+++ 03-basic-web-app/hello_world.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	// fmt.Println("Hello, world")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // first parameter is the url that I want this to listen to
-		fmt.Fprintf(w, "Hello, world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// first parameter is the url that I want this to listen to
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		// fmt.Fprintf(w, "Hello, world!")
 	})
 }
