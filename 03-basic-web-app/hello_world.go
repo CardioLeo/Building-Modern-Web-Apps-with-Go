d9b84150c98d38cef087340662946e1f4001bc13
CardioLeo <CardioLeo@gmail.com>
2025-01-25 01:02:38 -0700

    fleshes out handler function; about half-way through the video

--- 03-basic-web-app/hello_world.go
+++ 03-basic-web-app/hello_world.go
@@ -7,5 +7,7 @@ import (
 
 func main() {
 	// fmt.Println("Hello, world")
-	http.HandlerFunc()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // first parameter is the url that I want this to listen to
+		fmt.Fprintf(w, "Hello, world!")
+	})
 }
