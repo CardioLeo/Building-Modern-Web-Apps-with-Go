3a23ebdb0caa5020928f454acb402bfc1b73ad49
CardioLeo <CardioLeo@gmail.com>
2025-01-25 01:41:37 -0700

    creates addvalue function and calls it within about function/page

--- 03-basic-web-app/main.go
+++ 03-basic-web-app/main.go
@@ -10,6 +10,15 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page")
 }
 
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum)
+}
+
+func AddValues(x, y int) (int, error) {
+	return x + y
+}
+
 func main() {
 	// fmt.Println("Hello, world")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +34,7 @@ func main() {
 	})
 
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/", About)
 	// "how easy it is to start a webserver that listens in go"
 	_ = http.ListenAndServe(":8080", nil)
 	// starting with the underscore like that says, "if there's an error, I don't care"
