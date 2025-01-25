36f621ad7b30b987edb00d3ffb82bf3b9ab1f05f
CardioLeo <CardioLeo@gmail.com>
2025-01-25 01:12:18 -0700

    updates with printout of bytes written

--- 03-basic-web-app/hello_world.go
+++ 03-basic-web-app/hello_world.go
@@ -11,5 +11,9 @@ func main() {
 		// first parameter is the url that I want this to listen to
 		n, err := fmt.Fprintf(w, "Hello, world!")
 		// fmt.Fprintf(w, "Hello, world!")
+		// fmt.Println("Bytes written:" + n)
+		// "n is an int, and you can't print an int like that" - dude is being Dr Seuss
+
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
 	})
 }
