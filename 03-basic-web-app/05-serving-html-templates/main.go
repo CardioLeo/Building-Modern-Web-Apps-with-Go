25ff7ee346999823c1bd26d99f8fb05b95d37025
CardioLeo <CardioLeo@gmail.com>
2025-01-26 20:49:13 -0700

    minor changes to main; also main moved into new folder for lecture five of section three

--- 03-basic-web-app/05-serving-html-templates/main.go
+++ 03-basic-web-app/05-serving-html-templates/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"errors"
 	"html/template"
 )
 
@@ -13,17 +12,21 @@ const portNumber = ":8080"
 func renderTemplate(w http.ResponseWriter, tmpl string) {
 	// first of all parse the template
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err) // error messages aren't supposed to start with capital letters
+	}
 }
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // first thing that should appear before function is the function name,
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "about.page.tmpl")
 }
 // main is the main application function
 func main() {
