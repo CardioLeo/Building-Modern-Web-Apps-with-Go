fb53028038dcea6aada62bf0eb869f483890cd16
CardioLeo <CardioLeo@gmail.com>
2025-01-27 01:09:35 -0700

    updates config file with two new values from lecture 34; very small changes

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,5 +7,7 @@ import (
 // AppConfig holds the application config
 // anything needed site-wide can be added here
 type AppConfig struct {
+	UseCache bool
 	TemplateCache map[string]*template.Template
+	InfoLog *log.Logger
 }
