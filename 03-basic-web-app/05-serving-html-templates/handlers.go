549983487b8cfea4a2f911a4b015d6bff2fe75a6
CardioLeo <CardioLeo@gmail.com>
2025-01-26 21:27:20 -0700

    moves renderTemplate func into render.go - and amazingly it all still works!

--- 03-basic-web-app/05-serving-html-templates/handlers.go
+++ 03-basic-web-app/05-serving-html-templates/handlers.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-        // first of all parse the template
-        parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-        err := parsedTemplate.Execute(w, nil)
-        if err != nil {
-                fmt.Println("error parsing template:", err) // error messages aren't supposed to start with capital letters
-        }
-}
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
