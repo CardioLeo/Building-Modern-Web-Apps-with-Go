17ffd3c8af5c6942aad8f3bdaf50e2cc3666ee31
CardioLeo <CardioLeo@gmail.com>
2025-02-07 15:36:29 -0700

    takes templatedata out of handlers package and moves it into new package in pkg/models/templatedata.go

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -6,21 +6,6 @@ import (
 	"net/http"
 )
 
-// TemplateData holds data sent from handlers to templates
-type TemplateData struct {
-	StringMap map[string]string
-	IntMap map[string]int
-	FloatMap map[string]float32
-	Data map[string]interface{} //special data
-					// "and in Go, when you're not sure
-					// what the data type is, we call it
-					// an interface."
-	CSRFtoken string
-	Flash string
-	Warning string
-	Error string
-}
-
 // the repository pattern
 // allows swapping stuff out of code base with minimal changes
 
