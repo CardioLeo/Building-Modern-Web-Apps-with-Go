e87666053ca4d104e03d8a332835e42038e6e280
CardioLeo <CardioLeo@gmail.com>
2025-02-07 15:37:35 -0700

    adds comment about how this doc does not import other packages

--- pkg/models/templatedata.go
+++ pkg/models/templatedata.go
@@ -1,5 +1,8 @@
 package models
 
+// this will never import other packages; it will only exist to be imported
+// by packages other than itself.
+
 // TemplateData holds data sent from handlers to templates
 type TemplateData struct {
         StringMap map[string]string
