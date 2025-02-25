66a586126209798212b144fdc03015484695d1ff
CardioLeo <CardioLeo@gmail.com>
2025-02-24 23:07:56 -0700

    makes both home and about handler functions call renderTemplate; tests and it works

--- 03-basic-web-app/take-2/03-basic-web-app/02-serving-html-templates/main.go
+++ 03-basic-web-app/take-2/03-basic-web-app/02-serving-html-templates/main.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"fmt"
-	"errors"
 	"net/http"
+	"html/template"
 )
 
 const portNumber = ":8080"
 
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err) // error messages aren't supposed to begin with capital letters
+		return
+	}
+}
+
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request){
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the page handler
 func About(w http.ResponseWriter, r *http.Request){
-
+	renderTemplate(w, "about.page.tmpl")
 }
 
 // Main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil) // nil is for handler
