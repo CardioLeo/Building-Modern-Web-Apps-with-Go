f608e65fb13bae20e5449a436849c4ca71f06fb6
CardioLeo <CardioLeo@gmail.com>
2025-02-24 23:47:21 -0700

    updates render.go so that RenderTemplate is now exported, beginning now with a capital r

--- 03-basic-web-app/take-2/03-basic-web-app/04-go-mod-package-refactoring/pkg/render/render.go
+++ 03-basic-web-app/take-2/03-basic-web-app/04-go-mod-package-refactoring/pkg/render/render.go
@@ -6,7 +6,8 @@ import (
         "net/http"
 )
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
         parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
         err := parsedTemplate.Execute(w, nil)
         if err != nil {
