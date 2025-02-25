3cbf0f817674431eb090cf509e0358cbd6161ead
CardioLeo <CardioLeo@gmail.com>
2025-02-24 22:18:06 -0700

    creates Home handler function; deletes bulk of what was written two lectures ago, in lecture 21

--- 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
+++ 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
@@ -6,19 +6,11 @@ import (
 )
 
 func main() {
-	// instead of just printing hello world to the screen
-	// fmt.Println("Hello, world")
+	func Home(w http.ResponseWriter, r *http.Request){
+		
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		// Fprintf requires a ResponseWriter to write to
-		// returns an int and an error
-		// fmt.Println("Bytes written:", + n) // doesn't work for type int
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+
 	// uniform resource locator, url, -> "/"
 	_ = http.ListenAndServe(":8080", nil) // nil is for handler
 	// the underscore is for saying that if there's an error, we don't care
