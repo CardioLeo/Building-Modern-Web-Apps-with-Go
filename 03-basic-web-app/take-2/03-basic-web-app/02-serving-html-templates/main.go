49568b0292ab44186338dfc01424347b18abe4ea
CardioLeo <CardioLeo@gmail.com>
2025-02-24 22:54:11 -0700

    gets rid of a lot of code from about and home handlers, also deletes divide page and addvalues and divide values functions

--- 03-basic-web-app/take-2/03-basic-web-app/02-serving-html-templates/main.go
+++ 03-basic-web-app/take-2/03-basic-web-app/02-serving-html-templates/main.go
@@ -8,42 +8,13 @@ import (
 
 const portNumber = ":8080"
 
-// AddValues returns the sum of two integers passed into it
-func addValues(x, y int) int {
-        var sum int
-        sum = x + y
-        return sum
-}
-
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "This is the home page")
 }
 
 // About is the page handler
 func About(w http.ResponseWriter, r *http.Request){
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
-	// fmt.Fprintf(w, "This is the about page")
-}
 
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 0.0)
-	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-		return // if you don't have the return here, it just goes on to show the results
-		// as though you can actually divide by zero
-	}
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
-}
-
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
 }
 
 // Main is the main application function
