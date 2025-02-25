d99f82361c21346ead46ff91d0e3e44b1e065786
CardioLeo <CardioLeo@gmail.com>
2025-02-24 21:51:19 -0700

    follows video up past ten minute mark; just before it makes an http.ListenAndServe func

--- 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
+++ 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
@@ -9,8 +9,15 @@ func main() {
 	// instead of just printing hello world to the screen
 	// fmt.Println("Hello, world")
 
-	http.HandleFunc("/", func(w http.ResponseWriter(), r *http.Request){
-		fmt.Fprintf(w, "Hello, world!") // Fprintf requires a ResponseWriter to write to
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		// Fprintf requires a ResponseWriter to write to
+		// returns an int and an error
+		// fmt.Println("Bytes written:", + n) // doesn't work for type int
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
 	})
 	// uniform resource locator, url, -> "/"
 }
