0d1fb02ee2098a230b7e69975949adc4fdca3604
CardioLeo <CardioLeo@gmail.com>
2025-02-24 22:29:56 -0700

    makes code up to nine minute mark of lecture all work, compile and run in browser etc

--- 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
+++ 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
@@ -5,25 +5,29 @@ import (
 	"net/http"
 )
 
+// AddValues returns the sum of two integers passed into it
 func addValues(x, y int) int {
         var sum int
         sum = x + y
         return sum
 }
 
+// Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "This is the home page")
 }
 
+// About is the page handler
 func About(w http.ResponseWriter, r *http.Request){
 	sum := addValues(2, 2)
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 	// fmt.Fprintf(w, "This is the about page")
 }
 
+// Main is the main application function
 func main() {
-	http.HandlerFunc("/", Home)
-	http.HandlerFunc("/about", About)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	_ = http.ListenAndServe(":8080", nil) // nil is for handler
 	// the underscore is for saying that if there's an error, we don't care
