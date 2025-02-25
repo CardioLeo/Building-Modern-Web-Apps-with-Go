409060e32bc0878fcb868e5c0bcbff680b06450e
CardioLeo <CardioLeo@gmail.com>
2025-02-24 22:21:09 -0700

    adds new Home and About page functions outside of main; moved Home from within main to the outside; also creates dual http.HandleFunc calls for those two functions, but within main

--- 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
+++ 03-basic-web-app/take-2/03-basic-web-app/01-hello-world-web-app/main.go
@@ -5,13 +5,18 @@ import (
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "This is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "This is the about page")
+}
+
 func main() {
-	func Home(w http.ResponseWriter, r *http.Request){
-		
-	}
+	http.HandlerFunc("/", Home)
+	http.HandlerFunc("/about", About)
 
-
-	// uniform resource locator, url, -> "/"
 	_ = http.ListenAndServe(":8080", nil) // nil is for handler
 	// the underscore is for saying that if there's an error, we don't care
 	// because ListenAndServe does return an error, I guess
