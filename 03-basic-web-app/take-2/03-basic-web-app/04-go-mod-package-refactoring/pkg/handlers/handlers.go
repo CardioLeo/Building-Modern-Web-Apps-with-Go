ca7e8ef8c469818b79eb129bd618b6b2bddb7fd9
CardioLeo <CardioLeo@gmail.com>
2025-02-24 23:45:42 -0700

    updates handlers.go to call from render.go package

--- 03-basic-web-app/take-2/03-basic-web-app/04-go-mod-package-refactoring/pkg/handlers/handlers.go
+++ 03-basic-web-app/take-2/03-basic-web-app/04-go-mod-package-refactoring/pkg/handlers/handlers.go
@@ -6,11 +6,11 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request){
-        renderTemplate(w, "home.page.tmpl")
+        render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the page handler
 func About(w http.ResponseWriter, r *http.Request){
-        renderTemplate(w, "about.page.tmpl")
+        render.RenderTemplate(w, "about.page.tmpl")
 }
 
