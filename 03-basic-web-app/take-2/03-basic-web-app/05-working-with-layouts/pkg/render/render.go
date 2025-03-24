28541606a0e447bdac6f54b075dc664d10ebb508
echocslsk <CardioLeo@gmail.com>
2025-03-24 17:51:11 -0600

    fleshes out createTemplateCache function according to the first 13 minutes of video 31 of course

--- 03-basic-web-app/take-2/03-basic-web-app/05-working-with-layouts/pkg/render/render.go
+++ 03-basic-web-app/take-2/03-basic-web-app/05-working-with-layouts/pkg/render/render.go
@@ -26,7 +26,48 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 func createTemplateCache() (map[string]*template.Template, error) {
 	// myCache := make(map[string]*template.Template)
 	// one way you could do it
+	
 	myCache := map[string]*template.Template{}
 	// make it an empty map, same functionality
 	// but more common to see it this way
+
+	// get all of the files names *.page.tmpl from ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+	
+	// range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		// not full path, only name of template
+		name := filepath.Base(page)
+		// .Base() returns last element of the path
+		
+		ts, err := template.New(name).ParseFiles(page)
+		// ts stands for "templateSet", which maybe I should use instead
+		// setting ts to name, then parsing the files
+		if err != nil {
+			return myCache, err
+		}
+
+		// look for any layouts that exist in this directory
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		// gives slice of strings with layouts
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			// if matches greater than 0, deal with
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+		// stores current template set in name of map, myCache
+	}
+
+	return myCache, nil
 }
