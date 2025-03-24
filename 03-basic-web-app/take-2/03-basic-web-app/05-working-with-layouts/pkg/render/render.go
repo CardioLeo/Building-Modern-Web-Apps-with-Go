837b1fdb141cffc6b16d26a9cd90d83f143a1256
echocslsk <CardioLeo@gmail.com>
2025-03-24 17:32:00 -0600

    adds comments based on beginning of video 31; where we're going from here

--- 03-basic-web-app/take-2/03-basic-web-app/05-working-with-layouts/pkg/render/render.go
+++ 03-basic-web-app/take-2/03-basic-web-app/05-working-with-layouts/pkg/render/render.go
@@ -8,7 +8,14 @@ import (
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-        parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	// create a template cache
+
+	// get requested template from cache
+
+	// render the template
+
+
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
         err := parsedTemplate.Execute(w, nil)
         if err != nil {
                 fmt.Println("error parsing template:", err) // error messages aren't supposed to begin with capital letters
