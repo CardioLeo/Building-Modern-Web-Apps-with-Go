2d134725cc06343868f9283630709feeec041c4f
echocslsk <CardioLeo@gmail.com>
2025-03-24 20:12:42 -0600

    updates config.go with comments based on video 34, which adds two new variables to the AppConfig struct, but I have them commented out for now

--- 03-basic-web-app/take-2/03-basic-web-app/05-working-with-layouts/pkg/config/config.go
+++ 03-basic-web-app/take-2/03-basic-web-app/05-working-with-layouts/pkg/config/config.go
@@ -8,6 +8,9 @@ import (
 // will consist of a single struct
 // later, I can put anything that I need site-wide in this file
 type AppConfig struct {
-	// all that goes in here for now is Template Cache
+	// he adds this in video 34 (or 35, but made 34 retroactively?)
+	// UseCache bool
 	TemplateCache map[string]*template.Template
+	// InfoLog added in video 34, but commented out by me because unused for now
+	// InfoLog *log.Logger
 }
