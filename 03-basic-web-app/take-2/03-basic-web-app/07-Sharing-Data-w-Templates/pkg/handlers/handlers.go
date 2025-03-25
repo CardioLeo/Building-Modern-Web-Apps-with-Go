1d8779a541d2ccff9a2ca19f7b5fb08002c0201e
echocslsk <CardioLeo@gmail.com>
2025-03-24 21:53:26 -0600

    deletes TemplateData struct and comments now that they have been moved over to the new package, models, in templatedata.go file

--- 03-basic-web-app/take-2/03-basic-web-app/07-Sharing-Data-w-Templates/pkg/handlers/handlers.go
+++ 03-basic-web-app/take-2/03-basic-web-app/07-Sharing-Data-w-Templates/pkg/handlers/handlers.go
@@ -6,25 +6,6 @@ import (
 	"net/http"
 )
 
-// new type for any kind of data I might want to pass, video 37
-// TemplateData holds data from handlers to templates
-type TemplateData struct {
-	//
-	StringMap map[string]string
-	IntMap map[string]int
-	FloatMap map[string]float32
-	// special type for anything
-	// "and in Go, when you're not sure what the datatype is, you call it an interface"
-	Data map[string]interface{}
-	// the following is added just for now, so it doesn't have to be added later
-	// Cross-Site-Certificate-Forgery...
-	CSRFToken string
-	// for success message or whatever:
-	Flash string
-	Warning string
-	Error string
-}
-
 // using the repository pattern (!!)
 // allows swapping components out with minimal changes to code base
 
