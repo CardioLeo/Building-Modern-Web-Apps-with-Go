365b631e245fa11f60a21924b411affc509f232b
echocslsk <CardioLeo@gmail.com>
2025-03-24 20:47:25 -0600

    uncomments UseConfig bool in config.go which was created by me a couple videos ago, but only created by him just now

--- 03-basic-web-app/take-2/03-basic-web-app/06-Optimizing-Template-Cache/pkg/config/config.go
+++ 03-basic-web-app/take-2/03-basic-web-app/06-Optimizing-Template-Cache/pkg/config/config.go
@@ -8,8 +8,7 @@ import (
 // will consist of a single struct
 // later, I can put anything that I need site-wide in this file
 type AppConfig struct {
-	// he adds this in video 34 (or 35, but made 34 retroactively?)
-	// UseCache bool
+	UseCache bool // this is added for Repository Pattern, video 35
 	TemplateCache map[string]*template.Template
 	// InfoLog added in video 34, but commented out by me because unused for now
 	// InfoLog *log.Logger
