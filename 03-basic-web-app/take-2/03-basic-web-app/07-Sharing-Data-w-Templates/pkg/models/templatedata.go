f3d5032a5a942b093135ea4178faf5ba579b8564
echocslsk <CardioLeo@gmail.com>
2025-03-24 21:52:43 -0600

    moves TemplateData struct into new package called models, from handlers package

---
+++ 03-basic-web-app/take-2/03-basic-web-app/07-Sharing-Data-w-Templates/pkg/models/templatedata.go
@@ -0,0 +1,22 @@
+package models
+
+// TemplateData moved from handlers package in video 37
+
+// new type for any kind of data I might want to pass, video 37
+// TemplateData holds data from handlers to templates
+type TemplateData struct {
+        //
+        StringMap map[string]string
+        IntMap map[string]int
+        FloatMap map[string]float32
+        // special type for anything
+        // "and in Go, when you're not sure what the datatype is, you call it an interface"
+        Data map[string]interface{}
+        // the following is added just for now, so it doesn't have to be added later
+        // Cross-Site-Certificate-Forgery...
+        CSRFToken string
+        // for success message or whatever:
+        Flash string
+        Warning string
+        Error string
+}
