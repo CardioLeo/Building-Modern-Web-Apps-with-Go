971a25d5efd0cdce5a6ea465db2584e5adcf2188
echocslsk <CardioLeo@gmail.com>
2025-03-24 20:39:46 -0600

    adds repository pattern code to handlers.go

--- 03-basic-web-app/take-2/03-basic-web-app/06-Optimizing-Template-Cache/pkg/handlers/handlers.go
+++ 03-basic-web-app/take-2/03-basic-web-app/06-Optimizing-Template-Cache/pkg/handlers/handlers.go
@@ -5,6 +5,30 @@ import (
 	"net/http"
 )
 
+// using the repository pattern (!!)
+// allows swapping components out with minimal changes to code base
+
+// Repo, the repository used by the handlers
+// pointer to repository struct
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request){
         render.RenderTemplate(w, "home.page.tmpl")
