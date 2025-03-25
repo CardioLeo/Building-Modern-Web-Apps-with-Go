c980bdaab8e37b352b1c8d555d7bd4bd51586d12
echocslsk <CardioLeo@gmail.com>
2025-03-24 21:15:33 -0600

    adds comment to render.go regarding where I might replace a nil with something from template data struct created recently in video 37

--- 03-basic-web-app/take-2/03-basic-web-app/07-Sharing-Data-w-Templates/pkg/render/render.go
+++ 03-basic-web-app/take-2/03-basic-web-app/07-Sharing-Data-w-Templates/pkg/render/render.go
@@ -49,6 +49,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td handlers.TemplateData
 	
 	buf := new(bytes.Buffer) // for finer grained error checking!
 	err := t.Execute(buf, nil) // this will be where I use td from handlers.go, TemplateData
+	// first make sure there is data in there!
 	if err != nil {
 		log.Println(err)
 	}
