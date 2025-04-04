8b85f9b5fd769d048ebb6dfb77f49f96b633663c
echocslsk <CardioLeo@gmail.com>
2025-04-04 17:57:05 -0600

    brings code up to date with the first 3.5 minutees of video 02 of section 04; uses chi package instead of pat package; comments out pat pkg lines; runs go mod tidy in cli

--- 04-Improved-Routing-n-Middleware/01-using-pat-for-routing/cmd/web/routes.go
+++ 04-Improved-Routing-n-Middleware/01-using-pat-for-routing/cmd/web/routes.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	// "github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"github.com/CardioLeo/go-course/pkg/config"
 	"github.com/CardioLeo/go-course/pkg/handlers"
 	"net/http"
@@ -12,14 +13,23 @@ import (
 // this can now be seen in the go.mod file
 
 func routes(app *config.AppConfig) http.Handler {
-	// returns handler from http.Handler
-	mux := pat.New()
+	// // returns handler from http.Handler
+	// mux := pat.New()
 
-	// routes will be setup here
-	// routes will be taken out of main.go to be put here
+	// // routes will be setup here
+	// // routes will be taken out of main.go to be put here
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	// commenting everything out from pat so to use chi
+
+	mux := chi.NewRouter()
+
+	// create paths for NewRouter
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
