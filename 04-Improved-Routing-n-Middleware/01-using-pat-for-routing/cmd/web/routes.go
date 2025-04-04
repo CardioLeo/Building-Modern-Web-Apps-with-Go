7b0cb5cd8a529cec007749b2cfbecbde2eefa868
echocslsk <CardioLeo@gmail.com>
2025-04-04 17:15:22 -0600

    adds new go file in package main, routes.go, which abstracts the routes from main.go into this file so as to streamline use of mux from pat file; this commit also runs a cli command to get the external pat package from github to use it for this project; further added is a notes.txt file with the command specified, as well as a go.sum file; finally, routes.go has been filled out with 5-6 lines of code that previously were in main.go, and now have to be deleted from main.go

---
+++ 04-Improved-Routing-n-Middleware/01-using-pat-for-routing/cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/bmizerany/pat"
+	"github.com/CardioLeo/go-course/pkg/config"
+	"github.com/CardioLeo/go-course/pkg/config"
+	"net/http"
+)
+
+// ran the following command to make pat package accessible
+// go get github.com/bmizerany/pat
+// this can now be seen in the go.mod file
+
+func routes(app *config.AppConfig) http.Handler {
+	// returns handler from http.Handler
+	mux := pat.New()
+
+	// routes will be setup here
+	// routes will be taken out of main.go to be put here
+
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	return mux
+}
