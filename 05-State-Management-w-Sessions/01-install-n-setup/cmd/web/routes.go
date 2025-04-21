938458da6ad7c91e5754d7f8cb89f8c1228b06b8
echocslsk <CardioLeo@gmail.com>
2025-04-21 17:55:43 -0600

    adds a call to mux.Use for SessionLoad, as per end of video 01 of section 05

--- 05-State-Management-w-Sessions/01-install-n-setup/cmd/web/routes.go
+++ 05-State-Management-w-Sessions/01-install-n-setup/cmd/web/routes.go
@@ -14,14 +14,13 @@ import (
 
 func routes(app *config.AppConfig) http.Handler {
 	// // returns handler from http.Handler
-
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer) // middleware is built into chi's mux
-
 	mux.Use(WriteToConsole) // refers to function in middleware.go of pwd
 	// he deletes this call to WriteToConsole after using it for demonstration, but I'm keeping it for now
 	mux.Use(NoSurf) // calls the NoSurf package from middleware.go
+	mux.Use(SessionLoad) // refers to SessionLoad in middleware.go file, used for saving and loading session state
 
 	// create paths for NewRouter
 	mux.Get("/", handlers.Repo.Home)
