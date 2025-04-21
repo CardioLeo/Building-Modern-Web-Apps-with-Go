a2870dbb3d6f77dcf2190ef67e1a705ae9b42728
echocslsk <CardioLeo@gmail.com>
2025-04-21 17:59:04 -0600

    adds and updates comments to middleware.go

--- 05-State-Management-w-Sessions/01-install-n-setup/cmd/web/middleware.go
+++ 05-State-Management-w-Sessions/01-install-n-setup/cmd/web/middleware.go
@@ -6,17 +6,19 @@ import (
 	"github.com/justinas/nosurf"
 )
 
+// he deleted WriteToConsole at the end of 05/01, but I'm keeping it
+// WriteToConsole only does what it says when the page is "hit"
 func WriteToConsole(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		fmt.Println ("Hit the page")
 		next.ServeHTTP(w, r)
 	})
 }
-
 // every time you load the page, it runs this page and prints this out
 // he says, "and that's how you write middleware"
 
-// create No Surf protection from CSRF
+// NoSurf, creates No Surf protection from CSRF
+// adds said protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
@@ -29,7 +31,7 @@ func NoSurf(next http.Handler) http.Handler {
 	return csrfHandler
 }
 
-// SessionLoad helps to save session state
+// SessionLoad loads an saves the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
 }
